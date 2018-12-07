1efac25033896bde81130c69c34924794b6b1ba2
Hanna Svennevik <hannasv@student.matnat.uio.no>
2018-12-07 08:34:11 +0100

    correct files

--- downloading_data/download_temperature.py
+++ downloading_data/download_temperature.py
@@ -12,6 +12,8 @@ from ecmwfapi import ECMWFDataServer
 periode = "1979-01-01/to/2018-07-31"
 periode = "1990-12-01/to/1990-12-08"
 
+
+
 server = ECMWFDataServer()
 server.retrieve({
     "class": "ei",
@@ -19,14 +21,14 @@ server.retrieve({
     "expver": "1",
     "stream": "oper",
     "type": "an", # use fc for forcast data
-    "levtype": "sfc", # surface pressure
-    "param": "134.128", # surface pressure
+    "levtype": "sfc", #surface variable
+    "param": "167.128", #temperature
     "date": periode,
     "time": "00:00:00/06:00:00/12:00:00/18:00:00",
     #"step": "0", # timestep is 3 hours
-    "grid": "0.75/ 0.75", # 0.75, 0.75 is recomended resolution for grib, 1.0/1.0 is recomended for when you dont do global.
+    "grid": "0.75/0.75", # 0.75, 0.75 is recomended resolution for grib, 1.0/1.0 is recomended for when you dont do global.
     "area":"75/-15/30/42", #Europa --> N/W/S/E lat long degrees
     "format":"netcdf",
-    "target": "./filesML/pressure_Europa_sp.nc"
+    "target": "./filesML/temperature_Europa_sp.nc"
     #"resol":"av"
 })
--- downloading_data/relative_humidity_download.py
+++ downloading_data/relative_humidity_download.py
@@ -10,6 +10,7 @@ from ecmwfapi import ECMWFDataServer
 
 
 periode = "1979-01-01/to/2018-07-31"
+periode = "1990-12-01/to/1990-12-08"
 
 server = ECMWFDataServer()
 server.retrieve({
@@ -18,14 +19,14 @@ server.retrieve({
     "expver": "1",
     "stream": "oper",
     "type": "an", # use fc for forcast data
-    "levtype": "pl", # pressure level 
+    "levtype": "pl", # pressure level
     "param": "157.128",
     "date": periode,
     "time": "00:00:00/06:00:00/12:00:00/18:00:00",
     #"step": "0", # timestep is 3 hours
-    "grid": "1.0/1.0", # 0.75, 0.75 is recomended resolution for grib, 1.0/1.0 is recomended for when you dont do global.
+    "grid": "0.75/0.75", # 0.75, 0.75 is recomended resolution for grib, 1.0/1.0 is recomended for when you dont do global.
     "area":"75/-15/30/42", #Europa --> N/W/S/E lat long degrees
     "format":"netcdf",
-    "target": "./files/relative_humidity_Europe.nc"
+    "target": "./filesML/relative_humidity_Europa_sp.nc"
     #"resol":"av"
 })
