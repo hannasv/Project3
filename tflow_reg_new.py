1ec13698eb704970b73d277505c05d8833342046
paulina-t <paulinatedesco@gmail.com>
2018-12-17 00:39:40 +0100

    changed back to normal w

--- tflow_reg_new.py
+++ tflow_reg_new.py
@@ -2,7 +2,17 @@ import tensorflow as tf
 import numpy as np
 
 
-def nn_model(X, input_size, hidden_sizes, output_size, stddev):
+def nn_model(X, input_size, hidden_sizes, output_size, stddev=None):
+
+    """
+    :param X: features
+    :param input_size: number of features
+    :param hidden_sizes: number of nodes of the hidden layers, array-like
+    :param output_size: size of the output: 1
+    :param stddev: standard deviation for better elu and relu fits
+    :return:
+    """
+
     X = tf.cast(X, tf.float32)
     # Define weights and biases
     # (Using normal distribution)
@@ -12,41 +22,40 @@ def nn_model(X, input_size, hidden_sizes, output_size, stddev):
     b = []
     layer = []
 
-
     # 2) APPEND ALL THE LAYERS
 
     # Input layer
-    #w.append(tf.Variable(tf.random.normal([input_size, hidden_sizes[0]],
-                                       #mean=0.0,
-                                       #stddev=1.0,
-                                       #dtype=tf.float32,
-                                       #seed=None,
-                                       #name=None
-     #                                  )))
+    w.append(tf.Variable(tf.random.normal([input_size, hidden_sizes[0]],
+                                          mean=0.0,
+                                          stddev=1.0,
+                                          dtype=tf.float32,
+                                          seed=None,
+                                          name=None
+                                          )))
 
     b.append(tf.Variable(tf.zeros(hidden_sizes[0])))
 
-    w.append(tf.Variable(tf.truncated_normal([input_size, hidden_sizes[0]], stddev)))
-    #b.append(tf.Variable(tf.constant(0.1 if neuron_fn in [tf.nn.relu, tf.nn.relu6] else 0.0, shape=b_dims)))
+    # The following lines are best when using elu or relu
+    # w.append(tf.Variable(tf.truncated_normal([input_size, hidden_sizes[0]], stddev)))
+    # b.append(tf.Variable(tf.constant(0.1 if neuron_fn in [tf.nn.relu, tf.nn.relu6] else 0.0, shape=b_dims)))
 
     # add hidden layers (variable number)
     for i in range(1, len(hidden_sizes)):
-        #w.append(tf.Variable(tf.random_normal([hidden_sizes[i - 1], hidden_sizes[i]], stddev=0.1)))
+        # w.append(tf.Variable(tf.random_normal([hidden_sizes[i - 1], hidden_sizes[i]], stddev=0.1)))
         w.append(tf.Variable(tf.truncated_normal([hidden_sizes[i - 1], hidden_sizes[i]], stddev)))
         b.append(tf.Variable(tf.zeros([hidden_sizes[i]])))
 
-
     # Output layer
-    #w.append(tf.Variable(tf.random.normal([hidden_sizes[-1], output_size],
-     #                                  mean=0.0,
-      #                                 stddev=1.0,
-       #                                dtype=tf.float32,
-        #                               seed=None,
-         #                              name=None
-          #                             )))
+    w.append(tf.Variable(tf.random.normal([hidden_sizes[-1], output_size],
+                                          mean=0.0,
+                                          stddev=1.0,
+                                          dtype=tf.float32,
+                                          seed=None,
+                                          name=None
+                                          )))
 
     b.append(tf.Variable(tf.ones(output_size)))
-    w.append(tf.Variable(tf.truncated_normal([hidden_sizes[-1], output_size], stddev)))
+    # w.append(tf.Variable(tf.truncated_normal([hidden_sizes[-1], output_size], stddev)))
 
     # 3) DEFINE MODEL
 
@@ -58,5 +67,3 @@ def nn_model(X, input_size, hidden_sizes, output_size, stddev):
     output = tf.matmul(layer[-1], w[-1]) + b[-1]
 
     return output
-
-
